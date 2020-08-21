4561ad9ea006e41d0fdc7ee7a32e377299722934
RonavShah <67420914+RonavShah@users.noreply.github.com>
2020-08-21 16:59:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -49,6 +49,8 @@ function setup(){
 function draw(){
     if(backgroundImg)
         background(backgroundImg);
+    else{ background("black");
+        }
     
         noStroke();
         textSize(35)
